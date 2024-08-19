b0fa5efc5c0c660da3ef74c8b44930666ff8bcd2
Vladimir_T <lobster2@mail.ru>
2024-08-19 14:05:43 +0300

    Доработан сериализатор изображения

--- api_atlas_app/serializers.py
+++ api_atlas_app/serializers.py
@@ -1,4 +1,7 @@
+import os
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
 from .models import Placement, Image
@@ -6,11 +9,23 @@ from api_profile_app.serializers import ProfileUserSerializer
 from api_profile_app.models import User
 
 
+class ImageSerializer(ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['photo', 'description', 'placement']
+
+
+
 class PlacementSerializer(ModelSerializer):
     author = type('AuthorSerializer', (serializers.SerializerMethodField,
                                        ProfileUserSerializer), dict())(
                                        read_only=True,
                                        help_text='Автор')
+    images = type('ImagesSerializer', (serializers.SerializerMethodField,
+                                       ImageSerializer), dict())(
+                                        read_only=True,
+                                        help_text='Иллюстрация'
+    )
 
     class Meta:
         model = Placement
@@ -24,6 +39,18 @@ class PlacementSerializer(ModelSerializer):
             'images',
         ]
 
+    def validate(self, data):
+        photo = data.get('photo')
+        if photo:
+            ext = os.path.splitext(photo.name)[1].lower()
+            valid_extensions = ['.jpeg', 'jpg', '.pdf', '.png']
+            if ext not in valid_extensions:
+                raise ValidationError(
+                    {'error': 'Допустимые форматы: jpg, jpeg, pdf, png'},
+                    400)
+
+        return data
+
     @staticmethod
     def get_author(instance):
         author = User.objects.filter(placements=instance).first()
@@ -31,8 +58,8 @@ class PlacementSerializer(ModelSerializer):
             return ProfileUserSerializer(author).data
         return None
 
+    @staticmethod
+    def get_images(instance):
+        images = Image.objects.filter(placement=instance)
+        return  ImageSerializer(images, many=True).data
 
-class ImageSerializer(ModelSerializer):
-    class Meta:
-        model = Image
-        fields = '__all__'
